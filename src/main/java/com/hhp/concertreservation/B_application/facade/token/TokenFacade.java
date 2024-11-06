de6ebf44a5811b99eb491fc348ec148d6a9b342c
swnam <swnam@12cm.co.kr>
2024-11-06 19:02:53 +0900

    STEP7 facade 작업중. 토큰만 남음. 컨트롤러 수정만 남음
    근데 더 쪼개야 할까..?

--- src/main/java/com/hhp/concertreservation/B_application/facade/token/TokenFacade.java
+++ src/main/java/com/hhp/concertreservation/B_application/facade/token/TokenFacade.java
@@ -1,5 +1,10 @@
 package com.hhp.concertreservation.B_application.facade.token;
 
+import com.hhp.concertreservation.B_application.dto.token.IssueTokenInput;
+import com.hhp.concertreservation.B_application.dto.token.IssueTokenOutput;
+import com.hhp.concertreservation.B_application.dto.token.ReissueTokenInput;
+import com.hhp.concertreservation.B_application.dto.token.ReissueTokenOutput;
+import com.hhp.concertreservation.B_application.service.TokenService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -7,13 +12,32 @@ import org.springframework.stereotype.Service;
 @Service
 public class TokenFacade {
 
+    private final TokenService tokenService;
+
     /**
      * 대기열 토큰 발급
      */
+    public IssueTokenOutput issueToken(IssueTokenInput input) {
+
+        String token = tokenService.issueToken(input.getUserId(), input.getConcertId());
+
+        IssueTokenOutput output = IssueTokenOutput.builder()
+                .token(token)
+                .build();
+
+        return output;
+    }
 
     /**
      * 대기열 토큰 재발급
      */
+    public ReissueTokenOutput reissueToken(ReissueTokenInput input) {
+        ReissueTokenOutput token = tokenService.reissueToken(input.getUserId(), input.getConcertId());
 
+        ReissueTokenOutput output = ReissueTokenOutput.builder()
+                .token(token.getToken())
+                .build();
+        return output;
+    }
 
 }
--- src/main/java/com/hhp/concertreservation/B_application/service/TokenService.java
+++ src/main/java/com/hhp/concertreservation/B_application/service/TokenService.java
@@ -30,10 +30,10 @@ public class TokenService {
      * 토큰 발급
      */
     @Transactional
-    public IssueTokenOutput issueToken(IssueTokenInput input) {
+    public String issueToken(Long userId, Long concertId) {
         // TODO validation 이쁘게 바꾸기.
         // 현재 토큰 있는가
-        if (tokenRepository.existsTokenByUserId(input.getUserId())) {
+        if (tokenRepository.existsTokenByUserId(userId)) {
             //TODO EXCEPTION
             throw new RuntimeException("이미 존재하는 토큰");
         }
@@ -42,14 +42,14 @@ public class TokenService {
         //토큰 발급하고
         Token token = Token.builder()
                 .token(systemUuidHolder.random())
-                .userId(input.getUserId())
+                .userId(userId)
                 .expiredDate(systemClockHolder.plusFiveMinMillis())
                 .build();
         tokenRepository.save(token);
 
         // TODO validation 이쁘게 바꾸기.
         //대기열 확인해야함
-        Queue queue = queueRepository.findQueueByConcertId(input.getConcertId()).orElseThrow(
+        Queue queue = queueRepository.findQueueByConcertId(concertId).orElseThrow(
                 //TODO EXCEPTION
                 () -> new RuntimeException("해당하는 대기열이 없습니다.")
         );
@@ -67,24 +67,22 @@ public class TokenService {
 
         queueItemRepository.save(queueItem);
 
-        return IssueTokenOutput.builder()
-                .token(token.getToken())
-                .build();
+        return token.getToken();
     }
 
     /**
      * 토큰 재발급
      */
     @Transactional
-    public ReissueTokenOutput reissueToken(ReissueTokenInput input) {
+    public ReissueTokenOutput reissueToken(Long userId, Long concertId) {
         //기존 토큰 삭제
-        Token token = tokenRepository.findTokenByUserId(input.getUserId()).orElseThrow(
+        Token token = tokenRepository.findTokenByUserId(userId).orElseThrow(
                 //TODO EXCEPTION
                 () -> new RuntimeException("해당하는 토큰이 없습니다.")
         );
         tokenRepository.deleteTokenByTokenId(token.getId());
 
-        Queue queue = queueRepository.findQueueByConcertId(input.getConcertId()).orElseThrow(
+        Queue queue = queueRepository.findQueueByConcertId(concertId).orElseThrow(
                 //TODO EXCEPTION
                 () -> new RuntimeException("해당하는 대기열이 없습니다.")
         );
@@ -98,7 +96,7 @@ public class TokenService {
         //TODO 여기서 옮기자 따로 빼야할듯..?
         Token reToken = Token.builder()
                 .token(systemUuidHolder.random())
-                .userId(input.getUserId())
+                .userId(userId)
                 .expiredDate(systemClockHolder.plusFiveMinMillis())
                 .build();
         tokenRepository.save(reToken);
